20df101412a1e72141b802cd833a8a3c0d2325f9
Alexandr Ostroumov <m0nstrya9a@gmail.com>
2023-04-13 20:56:37 +0300

    add kod from lection

---
+++ main.py
@@ -0,0 +1,54 @@
+# print('введите первое число')
+# c = int(input())
+# print(c)
+# b = int(input('введите второе число: '))
+# print (c + b)
+
+
+# a=5.8945743
+# b=6.4546922
+# print(round(a*b,3))
+
+
+# iter = 2
+# iter += 3 # iter = iter +3
+# iter -= 4 # iter = iter -4
+
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура, это же Маша!')
+# elif username == 'Марина':
+#     print('Я так ждал Вас, Марина!')
+# elif username == 'Ильнар':
+#     print('Ильнар - ТОП')
+# else:
+#     print('Привет, ', username)
+
+
+# a = 'qwerty'
+
+# for i in a:
+#     print(i)
+    
+    
+text = 'СъЕШЬ ещё этих МяГкИх французских булок'
+# print(len(text))
+# print('ещё' in text) # выводит true, если слово есть в строке, 
+#                      # либо false если слова нет
+# print(text.lower())  # переводит в нижний регистр
+# print(text.upper())
+# print(text.replace('ещё','снова')) # заменяет одно слово другим
+# print(text[0]) # c
+# print(text[1]) # ъ
+# print(text[len(text)-1]) # к
+# print(text[-5]) # б
+# print(text[:]) # съешь ещё этих мягких французких булок
+# print(text[:2]) # съ
+# print(text[len(text)-2]) # о
+# print(text[2:9]) # ешь ещё
+# print(text[6:-18]) # ещё этих мягких
+# print(text[0:len(text):6]) # сеикакл
+# print(text[::6]) # сеикакл
+# text = text[2:9] + text[-5] + text[:2] # ЕШЬ ещёбСь
+# print(text)
