6aab9a00faebe657c768da68ac2748df427b7488
Alexandr Ostroumov <m0nstrya9a@gmail.com>
2023-04-21 17:06:31 +0300

    добавили листинги функций сортировки разделением и слиянием, функцию фибоначи и тд

--- main.py
+++ main.py
@@ -73,3 +73,76 @@
 #     return res
 # print(sum_str('q', 'e', 'l'))
 
+
+
+
+# import modul1 # импортируем модуль напрямую
+# print (modul1.max1(5,9))
+
+# from modul1 import max1 - таким образом можно импортировать одтельную функцию (max1 в данном случае)
+# print (max1(5,9)) - тогда вызываеть ее можно без обращения к конкретному файлу
+
+# from modul1 import *   -  таким образом можно импортировать все функции из файла
+
+# import modul1 as m1 #  импортируем наш модуль с сокращенным названием(просто чтобы меньше писать)
+# print(m1.max1(10,9)) # функцию вызываем используя сокращенное название модуля
+
+
+
+
+
+# def fib(n): # функция Фибоначи
+#     if n in [1,2]: # базис функции
+#         return 1
+#     return fib(n-1)+fib(n-2)
+
+# list_1 = []
+# for i in range(1,10):
+#     list_1.append(fib(i))
+# print (list_1)
+
+
+
+
+# def quick_sort(array): # сортировка разделением
+#     if len(array) <= 1:
+#         return array
+#     else:
+#         pivot = array [0]
+#     less = [i for i in array[1:] if i <= pivot]
+#     greater = [i for i in array[1:] if i > pivot]
+#     return quick_sort(less) + [pivot] + quick_sort(greater)
+
+# print(quick_sort([10,5,2]))
+
+
+
+# def merge_sort(nums):   # сортировка слиянием
+#     if len(nums) > 1:
+#         mid = len(nums) // 2
+#         left = nums[:mid]
+#         right = nums[mid:]
+#         merge_sort(left)
+#         merge_sort(right)
+#         i = j = k = 0
+#         while i < len(left) and j < len(right):
+#             if left[i] < right[j]:
+#                 nums[k] = left[i]
+#                 i +=1
+#             else:
+#                 nums[k] = right[j]
+#                 j +=1
+#             k += 1
+#         while i < len(left):
+#             nums[k] = left[i]
+#             i +=1
+#             k +=1
+            
+#         while j < len(right):
+#             nums[k] = right [j]
+#             j += 1
+#             k += 1
+            
+# list1 = [1,5,6,7,9,0,7,5,3]
+# merge_sort(list1)
+# print(list1)
