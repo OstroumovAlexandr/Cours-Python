d7505a27793ece87200bcc4d7afb2a0fb0fb446a
OstroumovAlexandr <120096952+OstroumovAlexandr@users.noreply.github.com>
2023-07-18 15:19:59 +0300

    Добавил файл ПРОЧТИ МЕНЯ

---
+++ test.py
@@ -0,0 +1,14 @@
+# def pow(a,b):
+#     return a ** b
+
+# if __name__ == '__main__':
+#     a = 5
+#     b = 2
+
+#     print(pow(a,b))
+#     print(__name__)
+
+# Архитектрура программы M      V       C (Modul View Controller)
+#                 Модуль MAIN --+-------+----------- обработка данных
+#                        Moдуль VIEW ---+----------- отображение всего
+#                                Модуль CONTROLLER - взаимодействие с пользователем и связывает модули View и Main
